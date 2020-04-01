339b05a68b14b66930f290dd5a2b59f9ea2769d7
James <james.abenj@gmail.com>
2020-04-01 12:37:23 -0400

    cleaned up files

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-Subscription.destroy_all
-
-Subscription.create(name: 'Netflix', amount: 10, frequency: "Monthly")
-Subscription.create(name: 'Hulu', amount: 6, frequency: "Monthly" )
-Subscription.create(name: 'Spotify', amount: 10, frequency: "Monthly")
