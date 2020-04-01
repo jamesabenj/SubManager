be35eb71e85416790e268e28d83677312e799f81
James <james.abenj@gmail.com>
2020-04-01 11:55:09 -0400

    define routes for signup, login, logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,44 @@
 class UsersController < ApplicationController
+
     get '/login' do 
         erb :'users/login'
     end 
 
+    get '/signup' do
+        erb :'/users/signup'
+    end 
+
     post '/login' do 
-        user = User.new(params)
-        if user.username.empty? || user.password.empty?
-            @error = "Please enter valid credentials"
-            erb :'users/login'
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:id] = user.id
+            redirect '/dashboard'
         else 
-            binding.pry
-            user.save
-            session[:user_id] = user.id
-            redirect '/subscriptions'
+            @error = "Please enter valid credentials."
+            erb :'users/login'
         end 
     end 
 
+    post '/signup' do
+        user = User.create(params)
+        if user.valid?
+          session[:id] = user.id
+          redirect to '/dashboard'
+        else
+          user.delete
+          @error = "Please enter valid credentials."
+          erb :'users/signup'
+        end
+      end
 
+    get '/dashboard' do
+        @user = User.find(session[:id])
+        erb :'/users/dashboard'
+    end 
 
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
 
 end 
