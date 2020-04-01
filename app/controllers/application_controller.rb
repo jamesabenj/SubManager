7209cd299f91956e64fd7a6ed2226e91fd68d843
James <james.abenj@gmail.com>
2020-04-01 11:49:17 -0400

    enable sessions, define helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,26 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  configure do
+    enable :sessions 
+    set :session_secret, "secret"
+  end 
+
   get "/" do
+    if logged_in?
+      redirect '/dashboard'
+    end
     erb :welcome
   end
+
+  helpers do
+    def current_user 
+      User.find_by(id: session[:id])
+    end
+
+    def logged_in? 
+     session[:id] ? true : false
+    end
+  end 
 
 end
